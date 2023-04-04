3a44435c8b93891fcda8d85036af1375175a352d
Chaslinux <97259120+chaslinux@users.noreply.github.com>
2023-04-03 21:15:37 -0400

    Initial commit of steam-list.sh
    This is the initial commit of the steam-list.sh script. Not much to see here.

---
+++ steam-list.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# steam-list.sh
+# list Steam games that are installed on a computer
+
+# by Charles McColm, chaslinux@gmail.com
+# Mastodon: @chaslinux@techhub.social
+
+# Create a .csv file of Steam games installed on a computer that can be imported into a spreadsheet
+
+# I wanted a list of all the Steam games I have installed on my desktop. I planned to use that list to
+# check compatibility with my laptop's specs. I figured I'd do that by importing the list of installed Steam
+# games into a spreadsheet. This script simply creates a .csv file of installed Steam games (not all the games
+# in a library, but installed games), which can be imported into a spreadsheet
+
+STEAMPATH="/home/$USER/.steam/steam/steamapps/common"
+DESKTOPPATH="/home/$USER/Desktop"
+ls -1 $STEAMPATH > $DESKTOPPATH/installed-steam-games.csv
